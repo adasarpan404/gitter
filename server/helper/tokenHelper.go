78ace2b1b87558b210f4d82744de6fe68251c044
Arpan Das <dasarpan471@gmail.com>
2024-05-25 09:48:49 +0530

    started working on auth

---
+++ server/helper/tokenHelper.go
@@ -0,0 +1,50 @@
+package helper
+
+import (
+	"time"
+
+	"github.com/adasarpan404/gitter/environment"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	ID        string `json:"id"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(email string, firstName string, lastName string, id string) (string, error) {
+	expirationTime := time.Now().Add(72 * time.Hour)
+	claims := &Claims{
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+		ID:        id,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(environment.SECRET_KEY))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func ValidateToken(signedToken string) (claim *Claims, msg string) {
+	token, err := jwt.ParseWithClaims(signedToken, &Claims{}, func(t *jwt.Token) (interface{}, error) { return []byte(environment.SECRET_KEY), nil })
+	if err != nil {
+		msg = err.Error()
+		return
+	}
+	claims, ok := token.Claims.(*Claims)
+	if !ok {
+		msg = "Error while getting Claims"
+		return
+	}
+	return claims, msg
+}
